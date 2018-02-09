56286d1594be3da7c7bb4a3423574de857f9bea2
Andrei Dore <andrei.dore@gmail.com>
2018-02-09 17:26:47 +0200

    Test code

--- src/main/java/com/github/andreidore/testsmartbillclient/Test.java
+++ src/main/java/com/github/andreidore/testsmartbillclient/Test.java
@@ -4,23 +4,36 @@ import java.util.List;
 
 import com.github.andreidore.smartbillclient.SeriesInfo;
 import com.github.andreidore.smartbillclient.SmartBillClient;
+import com.github.andreidore.smartbillclient.Tax;
 
 public class Test {
 
     public static void main(String[] args) {
 
-	SmartBillClient client = new SmartBillClient(System.getProperty("username"), System.getProperty("token"));
+	//testTaxes();
+	//testSeries();
 
-	/*
-	 * List<Tax> list = client.getTaxes(System.getProperty("cif"));
-	 * 
-	 * 
-	 * for (Tax t : list) { System.out.println(t); }
-	 */
+    }
+
+    public static void testSeries() {
+
+	SmartBillClient client = new SmartBillClient(System.getProperty("username"), System.getProperty("token"));
 
 	List<SeriesInfo> list2 = client.getSeries(System.getProperty("cif"));
 
 	for (SeriesInfo t : list2) {
+	    System.out.println(t);
+	}
+
+    }
+
+    public static void testTaxes() {
+
+	SmartBillClient client = new SmartBillClient(System.getProperty("username"), System.getProperty("token"));
+
+	List<Tax> list = client.getTaxes(System.getProperty("cif"));
+
+	for (Tax t : list) {
 	    System.out.println(t);
 	}
 
