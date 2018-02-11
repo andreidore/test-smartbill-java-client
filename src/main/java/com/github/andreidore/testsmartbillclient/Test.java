66583244774de0b7f3725548ae59bd8b83c83831
Andrei Dore <andrei.dore@gmail.com>
2018-02-11 22:41:28 +0200

    Tests

--- src/main/java/com/github/andreidore/testsmartbillclient/Test.java
+++ src/main/java/com/github/andreidore/testsmartbillclient/Test.java
@@ -1,9 +1,11 @@
 package com.github.andreidore.testsmartbillclient;
 
+import java.util.Date;
 import java.util.List;
 
 import com.github.andreidore.smartbillclient.SeriesInfo;
 import com.github.andreidore.smartbillclient.SmartBillClient;
+import com.github.andreidore.smartbillclient.Stock;
 import com.github.andreidore.smartbillclient.Tax;
 
 public class Test {
@@ -13,6 +15,8 @@ public class Test {
 	testTaxes();
 	System.out.println("---------------------------------------");
 	testSeries();
+	System.out.println("---------------------------------------");
+	testStock();
 
     }
 
@@ -30,7 +34,7 @@ public class Test {
     }
 
     public static void testTaxes() {
-	
+
 	System.out.println("Test getTaxes.");
 
 	SmartBillClient client = new SmartBillClient(System.getProperty("username"), System.getProperty("token"));
@@ -38,6 +42,20 @@ public class Test {
 	List<Tax> list = client.getTaxes(System.getProperty("cif"));
 
 	for (Tax t : list) {
+	    System.out.println(t);
+	}
+
+    }
+
+    public static void testStock() {
+
+	System.out.println("Test getTaxes.");
+
+	SmartBillClient client = new SmartBillClient(System.getProperty("username"), System.getProperty("token"));
+
+	List<Stock> list = client.getStocks(System.getProperty("cif"), new Date());
+
+	for (Stock t : list) {
 	    System.out.println(t);
 	}
 
